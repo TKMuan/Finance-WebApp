b6a5d414e77eb357e25c539060508eaa743f0ef1
TKMuan <guitethangkhanmuan@gmail.com>
2025-11-23 18:53:58 +0530

    Update error handling module
    - Add MissingFieldError exception class
    - Improve error message handling

--- backend/src/errors.py
+++ backend/src/errors.py
@@ -1,14 +1,57 @@
+from enum import Enum
+from typing import Type, Union
+
+class ErrorCodes:
+    class BASE(Enum):
+        ERROR = "400"
+
+    class JWT(Enum):
+        REQUIRE_REFRESH = "401"
+        REQUIRE_RE_AUTH = "402"
+        EXPIRED_TOKEN = "403"
+        MISSING_TOKEN = "404"
+        INVALID_TOKEN = "405"
+
+    class DOCUMENT(Enum):
+        RECORD_NOT_FOUND = '401'
+        MISSING_MODIFYING_USER = '402'
+        UNAUTHORIZED_ACCESS = '403'
+
+    class ACCOUNT(Enum):
+        INVALID_CREDENTIALS = "404"
+
+
+class SuccessCodes:
+    class BASE(Enum):
+        SUCCESS = "200"
+
+    class JWT(Enum):
+        ACCESS_REFRESHED = "201"
+        ACCESS_ACTIVE = '202'
+        VALID_TOKEN = '203'
+        REFRESH_SUCCESS = '204'
+
+    class DOCUMENT(Enum):
+        UPDATED = "201"
+
+SuccessTypes = Union[SuccessCodes.JWT, SuccessCodes.DOCUMENT, SuccessCodes.BASE]
+ErrorTypes = Union[ErrorCodes.DOCUMENT, ErrorCodes.JWT, ErrorCodes.ACCOUNT, ErrorCodes.BASE]
+
+class JWTError(Exception):
+    def __init__(self, message: str=None, code: Union[ErrorCodes.JWT, ErrorCodes.BASE]=None):
+        if code is None:
+            code = ErrorCodes.BASE.ERROR
+        if message is None:
+            message = "An error occurred with JWT processing."
+        super().__init__(message)
+        self.code = code
+
 class MissingFieldError(ValueError):
     def __init__(self, message=None, fields=None):
         if message is None:
             message = f"Required field(s) {fields}  are missing."
         super().__init__(message)
 
-class JWTError(Exception):
-    def __init__(self, message=None):
-        if message is None:
-            message = "An error occurred with JWT processing."
-        super().__init__(message)
 
 class AuthenticationError(Exception):
     def __init__(self, message=None):
@@ -33,6 +76,7 @@ class MissingModifyingUser(AccountError):
         if message is None:
             message = "Modifying user information is required."
         super().__init__(message)
+        self.code = ErrorCodes.DOCUMENT.MISSING_MODIFYING_USER
 
 class DocumentError(Exception):
     def __init__(self, message=None):
@@ -43,7 +87,7 @@ class DocumentError(Exception):
 class MissingDocumentID(DocumentError):
     def __init__(self, message=None):
         if message is None:
-            message = "Valid Document ID is required."
+            message = "Valid DOCUMENT ID is required."
         super().__init__(message)
 
 class UnauthorizedAccess(PermissionError):
